63c2e4d880831598b8d8bcd5a23022669f2ed148
mubarizra@code.edu.az <mubarizra@code.edu.az>
2023-05-03 07:42:56 +0400

    Task

---
+++ EntityFramework-Slider/EntityFramework-Slider/Controllers/AccountController.cs
@@ -0,0 +1,107 @@
+﻿using EntityFramework_Slider.Models;
+using EntityFramework_Slider.ViewModels.Account;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace EntityFramework_Slider.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signManager;
+        public AccountController(UserManager<AppUser> userManager,
+                                 SignInManager<AppUser> signManager)
+        {
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+
+        [HttpGet]
+         public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            AppUser newUser = new()
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                FullName = model.FullName,
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(newUser,model.Password);
+
+            if(!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty,item.Description);
+                }
+                
+                return View(model);
+            }
+
+
+            return RedirectToAction(nameof(Login));
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            AppUser user = await _userManager.FindByEmailAsync(model.EmailOrUsername);
+
+            if (user is null)
+            {
+                user = await _userManager.FindByNameAsync(model.EmailOrUsername);
+            }
+
+            if (user is null)
+            {
+                ModelState.AddModelError(string.Empty, "Email or password is wrong");
+                return View(model);
+            }
+
+            var result = await _signManager.PasswordSignInAsync(user, model.Password, false, false);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Email or password is wrong");
+                return View(model);
+            }
+
+            return RedirectToAction("Index","Home");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
